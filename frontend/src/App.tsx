53d62fde1ac99e775dba67cfe12f7275193b6dee
Michelle Li <michelleyli123@gmail.com>
2023-04-16 18:19:28 -0400

    frontend react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./static/images/logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="full-body-container">
+      <img className="logo" src={logo} alt="Fiend for Beans logo" />{" "}
+      <label>Enter which flavor profiles you enjoy.</label>
+      <div className="input-box">
+        <div>
+          <input placeholder="citrus, floral, sweet" id="filter-text-val" />
+        </div>
+      </div>
+      <label>Select which roast you prefer.</label>
+      <div className="dropdown">
+        <select name="roast" id="roast_select">
+          <option value="Light" selected>
+            Light
+          </option>
+          <option value="Medium-Light">Medium-Light</option>
+          <option value="Medium">Medium</option>
+          <option value="Medium-Dark">Medium-Dark</option>
+          <option value="Dark">Dark</option>
+        </select>
+      </div>
+      <br />
+      <button id="search-button">Search</button>
+      <div id="answer-box"></div>
     </div>
   );
 }
